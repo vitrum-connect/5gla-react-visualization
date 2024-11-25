9ea2aa09f248afd68b08bf89ed3cf9a68b7f3203
THS <ths@lmis.de>
2024-11-25 15:40:14 +0100

    - added: hello world page

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HelloWorld.css';
+
+function HelloWorld() {
+  return (
+      <p>Hello World!</p>
+  );
+}
+
+export default HelloWorld;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import HelloWorld from './HelloWorld';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
