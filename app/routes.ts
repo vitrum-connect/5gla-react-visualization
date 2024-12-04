a218b0d5aa44b681ef41054f1d047cf4354998f5
THS <ths@lmis.de>
2024-12-04 10:06:07 +0100

    - removed: example start page

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,5 @@
-import {index, route, RouteConfig} from '@react-router/dev/routes';
+import {route, RouteConfig} from '@react-router/dev/routes';
 
 export default [
-    index('./routes/HelloWorld.tsx'),
     route('impressum', './routes/Impressum.tsx')
 ] satisfies RouteConfig;
