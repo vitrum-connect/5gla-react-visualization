e3005cd2fd9fb6a58e50c0e48fac6e984084f6f1
THS-LMIS <91137979+THS-LMIS@users.noreply.github.com>
2024-12-16 08:36:41 +0100

    Hinzufügen von Funktionalitäten der Kartendarstellung auf der Startseite (#14)
    Co-authored-by: Thomas Schley <thomas.schley@lmis.de>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import {reactRouter} from '@react-router/dev/vite';
 import autoprefixer from 'autoprefixer';
-import {defineConfig} from 'vite';
+import {ConfigEnv, defineConfig, loadEnv, UserConfig} from 'vite';
 
-export default defineConfig({
-    css: {
-        postcss: {
-            plugins: [autoprefixer]
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        css: {
+            postcss: {
+                plugins: [autoprefixer]
+            }
+        },
+        plugins: [reactRouter()],
+        server: {
+            proxy: {
+                '/v2/entities': env.VITE_FIWARE_SERVER_BASE_URL
+            }
         }
-    },
-    plugins: [reactRouter()]
+    }
 });
